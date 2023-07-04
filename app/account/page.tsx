dfe5bbd04496a5f23640968c443607b7380a551f
Anton Babentsov <ababentsov@gmail.com>
2023-07-04 20:24:50 +0200

    Account: subscription list

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,9 +2,11 @@ import ManageSubscriptionButton from './ManageSubscriptionButton';
 import {
   getSession,
   getUserDetails,
-  getSubscription
+  getSubscription,
+  getTenants
 } from '@/app/supabase-server';
 import Button from '@/components/ui/Button';
+import Input from '@/components/ui/Input';
 import { Database } from '@/types_db';
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
 import { revalidatePath } from 'next/cache';
@@ -12,13 +14,27 @@ import { cookies } from 'next/headers';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
-import Input from '@/components/ui/Input'
 
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(' ');
+}
+
+function formatDate(dateString:any) {
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  });
+  return formattedDate;
+}
 
 export default async function Account() {
   const [session, subscription] = await Promise.all([
     getSession(),
-    getSubscription()
+    getTenants()
   ]);
 
 
@@ -30,31 +46,31 @@ export default async function Account() {
     return redirect('/signin');
   }
 
-  async function getUserInfo (user:any) {
+  async function getUserInfo(user: any) {
     'use server';
     const supabase = createServerActionClient<Database>({ cookies });
     const { data, error } = await supabase
       .from('users')
       .select('*')
       .eq('id', user?.id)
-      .single()
+      .single();
     if (error) {
       console.log(error);
     }
-  
+
     return data;
   }
 
   // const userInfo = await getUserInfo(user)
-  const userInfo = await getUserDetails(user?.id)
+  const userInfo = await getUserDetails(user?.id);
 
-  const subscriptionPrice = (data:any) =>{
+  const subscriptionPrice = (data: any) => {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: data?.prices?.currency!,
       minimumFractionDigits: 0
     }).format((data?.prices?.unit_amount || 0) / 100);
-  }
+  };
 
   // console.log(subscription)
   const updateName = async (formData: FormData) => {
@@ -86,106 +102,152 @@ export default async function Account() {
     revalidatePath('/account');
   };
 
-
-
   return (
     <section className="mb-32 dark:bg-black dark:text-white bg-white text-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
-        <div className="sm:align-center sm:flex sm:flex-col">
-          <h1 className="text-4xl font-extrabold sm:text-center sm:text-6xl">
-            Account 
-          </h1>
-          <p className="max-w-2xl m-auto mt-5 text-xl sm:text-center sm:text-2xl">
-            We partnered with Stripe for a simplified billing.
-          </p>
-        </div>
+        <div className="sm:align-center sm:flex sm:flex-col"></div>
       </div>
       <Card
-          title="Your Name"
-          description="Please enter your full name, or a display name you are comfortable with."
-          footer={
-            <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
-              <p className="pb-4 sm:pb-0">64 characters maximum</p>
-              <Button
-                variant="slim"
-                type="submit"
-                form="nameForm"
-              >
-                {/* WARNING - In Next.js 13.4.x server actions are in alpha and should not be used in production code! */}
-                Update Name
-              </Button>
-            </div>
-          }
-        >
-          <div className="mt-8 mb-4 text-xl font-semibold">
-            <form id="nameForm" action={updateName}>
-              <Input              
-                type="text"
-                name="name"    
-                label="Name"        
-                defaultValue={userInfo?.full_name ?? ''}
-                placeholder="Your name"
-                maxLength={64} />              
-            </form>
+        title="Your Name"
+        description="Please enter your full name, or a display name you are comfortable with."
+        footer={
+          <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
+            <p className="pb-4 sm:pb-0">64 characters maximum</p>
+            <Button variant="slim" type="submit" form="nameForm">
+              {/* WARNING - In Next.js 13.4.x server actions are in alpha and should not be used in production code! */}
+              Update Name
+            </Button>
           </div>
-        </Card>
-        <Card
-          title="Your Email"
-          description="Please enter the email address you want to use to login."
-          footer={
-            <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
-              <p className="pb-4 sm:pb-0">
-                We will email you to verify the change.
-              </p>
-              <Button
-                variant="slim"
-                type="submit"
-                form="emailForm"
-                // disabled={true}
-              >
-                Update Email
-              </Button>
-            </div>
-          }
-        >
-          <div className="mt-8 mb-4 text-xl font-semibold">
-            <form id="emailForm" action={updateEmail}>
-              <Input 
-                type="text"
-                name="email"
-                className="w-1/2 p-3 rounded-md bg-zinc-800"
-                defaultValue={user ? user.email : ''}
-                placeholder="Your email"
-                maxLength={64}
-              />
-            </form>
+        }
+      >
+        <div className="mt-8 mb-4 text-xl font-semibold">
+          <form id="nameForm" action={updateName}>
+            <Input
+              type="text"
+              name="name"
+              label="Name"
+              defaultValue={userInfo?.full_name ?? ''}
+              placeholder="Your name"
+              maxLength={64}
+            />
+          </form>
+        </div>
+      </Card>
+      <Card
+        title="Your Email"
+        description="Please enter the email address you want to use to login."
+        footer={
+          <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
+            <p className="pb-4 sm:pb-0">
+              We will email you to verify the change.
+            </p>
+            <Button
+              variant="slim"
+              type="submit"
+              form="emailForm"
+              // disabled={true}
+            >
+              Update Email
+            </Button>
           </div>
-        </Card>
-      {subscription && subscription.map((data:any, index:any)=>
-        <>
-          <div className="p-4">
-        <Card
-          title="Your Plan"
-          description={
-            data
-              ? `You are currently on the ${data?.prices?.products?.name} plan.`
-              : 'You are not currently subscribed to any plan.'
-          }
-          footer={<ManageSubscriptionButton session={session} />}
-        >
-          <div className="mt-8 mb-4 text-xl font-semibold">
-            {subscription ? (
-              `${subscriptionPrice(data)}/${data?.prices?.interval}`
-            ) : (
-              <Link href="/">Choose your plan</Link>
-            )}
-          </div>
-        </Card>
+        }
+      >
+        <div className="mt-8 mb-4 text-xl font-semibold">
+          <form id="emailForm" action={updateEmail}>
+            <Input
+              type="text"
+              name="email"
+              className="w-1/2 p-3 rounded-md bg-zinc-800"
+              defaultValue={user ? user.email : ''}
+              placeholder="Your email"
+              maxLength={64}
+            />
+          </form>
+        </div>
+      </Card>
 
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle">
+            <table className="min-w-full border-separate border-spacing-0">
+              <thead className="">
+                <tr className="">
+                  <th className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
+                    Name
+                  </th>
+                  <th className="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell">
+                    Tenants
+                  </th>
+                  <th className="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell">
+                    Capacity
+                  </th>
+                  <th className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-center text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter">
+                    Status
+                  </th>
+                  <th className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {subscription &&
+                  subscription.map((data: any, index: any) => {
+                  console.log(data)
+                  return (
+                    <>
+                      <tr key={index}>
+                        <td
+                          className={classNames(
+                            index !== subscription.length - 1
+                              ? 'border-b border-gray-200'
+                              : '',
+                            'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8 dark:text-white'
+                          )}
+                        >
+                          {data?.domain.name}.{data?.domain.host}
+                        </td>
+                        <td
+                          className={classNames(
+                            index !== subscription.length - 1
+                              ? 'border-b border-gray-200'
+                              : '',
+                            'whitespace-nowrap px-3 py-4 text-sm text-gray-500'
+                          )}
+                        >
+                          <div className="flex items-center">
+                                    <div className="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> Online
+                                </div>
+                        </td>
+                        <td
+                          className={classNames(
+                            index !== subscription.length - 1
+                              ? 'border-b border-gray-200'
+                              : '',
+                            'whitespace-nowrap px-3 py-4 text-sm text-gray-500'
+                          )}
+                        >
+                          {formatDate(data.created_at)}
+                        </td>
+                        <td
+                          className={classNames(
+                            index !== subscription.length - 1
+                              ? 'border-b border-gray-200'
+                              : '',
+                            'relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-8 lg:pr-8'
+                          )}
+                        >
+                                  <Link href={"/tenant/" + data.id}>
+                                    <span data-modal-toggle="editUserModal" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Details</span>                  
+                                </Link>
+                        </td>
+                      </tr>
+                    </>
+                  )})}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-        </>
-      )}
- 
     </section>
   );
 }
